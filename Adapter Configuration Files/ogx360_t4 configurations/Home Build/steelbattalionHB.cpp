c0ba8bf859911efdd5a3663e9e6a04c77f55da96
quizerno <43489654+quizerno@users.noreply.github.com>
2026-09-24 15:57:31 -0400

    Refactor Steel Battalion input configuration
    Updated Steel Battalion input configuration for ogx360_t4. Added new key mappings and modified existing ones for better control.

---
+++ Adapter Configuration Files/ogx360_t4 configurations/Home Build/steelbattalionHB.cpp
@@ -0,0 +1,255 @@
+#include <Arduino.h>
+#include <tusb.h>
+#include <USBHost_t36.h>
+#include "xid.h"
+
+//Steel Battalion Home Build Input Configuration for ogx360_t4
+//Provided by Quizerno
+//This build will provide the settings for a MIDDLE BLOCK represented by a full length keyboard
+//For building, you must
+//1. Rename this file to steelbattalion.cpp and put it in the src folder
+//2. You MUST also include the altered main.cpp file
+
+//USB Device Interface
+static USB_SteelBattalion_InReport_t sb_data;
+static USB_SteelBattalion_OutReport_t sb_feedback;
+static uint8_t kcode[256];
+
+static void key_pressed_cb(uint8_t keycode)
+{
+    TU_LOG1("Key pressed %u\r\n", keycode);
+    kcode[keycode] = 0xFF;
+}
+
+static void key_released_cb(uint8_t keycode)
+{
+    TU_LOG1("Key released %u\r\n", keycode);
+    kcode[keycode] = 0x00;
+}
+
+static uint8_t is_key_pressed(uint16_t k, uint8_t clear)
+{
+    uint8_t key = k & 0xFF;
+    uint8_t pressed = kcode[key];
+
+    //If clear, the input will be cleared. This allows event based handling
+    if (clear)
+        kcode[key] = 0x00;
+
+    return pressed;
+}
+
+void steelbattalion_init(KeyboardController *kb, MouseController *m, JoystickController *joy)
+{
+    memset(&sb_data, 0x00, sizeof(sb_data));
+    sb_data.bLength = sizeof(sb_data);
+    sb_data.gearLever = 7;
+    kb->attachRawPress(key_pressed_cb);
+    kb->attachRawRelease(key_released_cb);
+}
+
+void steelbattalion_task(uint8_t type_index, KeyboardController *kb, MouseController *m, JoystickController *joy)
+{
+    uint8_t index = xid_get_index_by_type(type_index, XID_TYPE_STEELBATTALION);
+
+    if (xid_send_report_ready(index))
+    {
+        sb_data.dButtons[0]   = 0x0000;
+        sb_data.dButtons[1]   = 0x0000;
+        sb_data.dButtons[2]  &= 0xFFFC; //Need to only clear the two LSBs. The other bits are the toggle switches
+
+
+      //Leftovers from keyboard configuration
+       // sb_data.rotationLever = 0x0000;
+       // sb_data.sightChangeX  = 0x0000;
+       // sb_data.sightChangeY  = 0x0000;
+      //  sb_data.rightPedal    = 0x0000;
+      //  sb_data.middlePedal   = 0x0000;
+      //  sb_data.leftPedal     = 0x0000;
+
+
+      //Analog Keys to be rebound
+      //  if (sb_data.aimingX > 50000)          sb_data.sightChangeX = 8000;
+      //  if (sb_data.aimingX < 14000)          sb_data.sightChangeX = -8000;
+      //  if (sb_data.aimingY > 50000)          sb_data.sightChangeY = 8000;
+      //  if (sb_data.aimingY < 14000)          sb_data.sightChangeY = -8000;
+
+      //unchanged from default config
+        if (is_key_pressed(KEY_W, 0))         sb_data.rightPedal  = 0xFFFF;
+        if (is_key_pressed(KEY_S, 0))         sb_data.middlePedal = 0xFFFF;
+        if (is_key_pressed(KEY_SPACE, 0))     sb_data.leftPedal = 0xFFFF;
+        if (is_key_pressed(KEY_A, 0))         sb_data.rotationLever = INT16_MIN;
+        if (is_key_pressed(KEY_D, 0))         sb_data.rotationLever = INT16_MAX;
+
+
+    //Digital Gear Shifter
+        if (is_key_pressed(KEY_TAB, 1))         sb_data.gearLever -= (sb_data.gearLever > 7) ? 1 : 0;
+        if (is_key_pressed(KEY_LEFTSHIFT, 1))         sb_data.gearLever += (sb_data.gearLever < 13) ? 1 : 0;
+
+
+            if (is_key_pressed(KEY_E, 1))    sb_data.tunerDial += (sb_data.tunerDial < 15) ? 1 : -15;
+            if (is_key_pressed(KEY_Q, 1))  sb_data.tunerDial -= (sb_data.tunerDial > 0) ? 1 : -15;
+            if (is_key_pressed(KEY_1, 0))     sb_data.dButtons[1] |= CXBX_SBC_GAMEPAD_W1_COMM1;
+            if (is_key_pressed(KEY_2, 0))     sb_data.dButtons[1] |= CXBX_SBC_GAMEPAD_W1_COMM2;
+            if (is_key_pressed(KEY_3, 0))     sb_data.dButtons[1] |= CXBX_SBC_GAMEPAD_W1_COMM3;
+            if (is_key_pressed(KEY_4, 0))     sb_data.dButtons[1] |= CXBX_SBC_GAMEPAD_W1_COMM4;
+            if (is_key_pressed(KEY_5, 0))     sb_data.dButtons[2] |= CXBX_SBC_GAMEPAD_W2_COMM5;
+
+      
+
+      //Maintenance Buttons
+        if (is_key_pressed(KEY_I, 0))                sb_data.dButtons[1] |= CXBX_SBC_GAMEPAD_W1_WEAPONCONMAIN; //
+        if (is_key_pressed(KEY_O, 0))           sb_data.dButtons[1] |= CXBX_SBC_GAMEPAD_W1_WEAPONCONSUB; //
+        if (is_key_pressed(KEY_P, 0))         sb_data.dButtons[1] |= CXBX_SBC_GAMEPAD_W1_WEAPONCONMAGAZINE; //
+        if (is_key_pressed(KEY_9, 0))         sb_data.dButtons[1] |= CXBX_SBC_GAMEPAD_W1_EXTINGUISHER; //
+        if (is_key_pressed(KEY_0, 0))       sb_data.dButtons[1] |= CXBX_SBC_GAMEPAD_W1_CHAFF; //
+        if (is_key_pressed(KEY_8, 0))         sb_data.dButtons[1] |= CXBX_SBC_GAMEPAD_W1_WASHING; //
+      //Function Buttons
+        if (is_key_pressed(KEY_KP7, 0))        sb_data.dButtons[1] |= CXBX_SBC_GAMEPAD_W1_FUNCTIONF1;//
+        if (is_key_pressed(KEY_KP4, 0))        sb_data.dButtons[1] |= CXBX_SBC_GAMEPAD_W1_FUNCTIONF2;//
+        if (is_key_pressed(KEY_KP1, 0))        sb_data.dButtons[1] |= CXBX_SBC_GAMEPAD_W1_FUNCTIONF3; //
+        if (is_key_pressed(KEY_KP8, 0))        sb_data.dButtons[1] |= CXBX_SBC_GAMEPAD_W1_FUNCTIONTANKDETACH; //
+        if (is_key_pressed(KEY_KP5, 0))        sb_data.dButtons[1] |= CXBX_SBC_GAMEPAD_W1_FUNCTIONOVERRIDE; //
+        if (is_key_pressed(KEY_KP2, 0))        sb_data.dButtons[1] |= CXBX_SBC_GAMEPAD_W1_FUNCTIONNIGHTSCOPE; //
+        if (is_key_pressed(KEY_KP9, 0))        sb_data.dButtons[0] |= CXBX_SBC_GAMEPAD_W0_FUNCTIONFSS; //
+        if (is_key_pressed(KEY_KP6, 0))        sb_data.dButtons[0] |= CXBX_SBC_GAMEPAD_W0_FUNCTIONMANIPULATOR; //
+        if (is_key_pressed(KEY_KP3, 0))        sb_data.dButtons[0] |= CXBX_SBC_GAMEPAD_W0_FUNCTIONLINECOLORCHANGE;//
+//move modifier
+
+            if (is_key_pressed(KEY_F1, 1))     sb_data.dButtons[2] ^= CXBX_SBC_GAMEPAD_W2_TOGGLEOXYGENSUPPLY;
+            if (is_key_pressed(KEY_F2, 1))     sb_data.dButtons[2] ^= CXBX_SBC_GAMEPAD_W2_TOGGLEFILTERCONTROL;
+            if (is_key_pressed(KEY_F3, 1))     sb_data.dButtons[2] ^= CXBX_SBC_GAMEPAD_W2_TOGGLEFUELFLOWRATE;
+            if (is_key_pressed(KEY_F4, 1))     sb_data.dButtons[2] ^= CXBX_SBC_GAMEPAD_W2_TOGGLEBUFFREMATERIAL;
+            if (is_key_pressed(KEY_F5, 1))     sb_data.dButtons[2] ^= CXBX_SBC_GAMEPAD_W2_TOGGLEVTLOCATION;
+            //if (is_key_pressed(KEY_RIGHT, 1)) sb_data.dButtons[2] |=  0xFFFC; //All toggles on
+            //if (is_key_pressed(KEY_LEFT, 1))  sb_data.dButtons[2] &= ~0xFFFC; //All tolgges off
+            if (is_key_pressed(KEY_LEFT, 0))  sb_data.sightChangeX = INT16_MIN;
+            if (is_key_pressed(KEY_RIGHT, 0)) sb_data.sightChangeX = INT16_MAX;
+            if (is_key_pressed(KEY_UP, 0))    sb_data.sightChangeY = INT16_MIN;
+            if (is_key_pressed(KEY_DOWN, 0))  sb_data.sightChangeY = INT16_MAX;
+  
+        
+        if (is_key_pressed(KEY_PAUSE, 0))       sb_data.dButtons[0] |= CXBX_SBC_GAMEPAD_W0_EJECT;
+        if (is_key_pressed(KEY_KPMINUS, 0))         sb_data.dButtons[0] |= CXBX_SBC_GAMEPAD_W0_COCKPITHATCH;
+        if (is_key_pressed(KEY_KPPLUS, 0)) sb_data.dButtons[0] |= CXBX_SBC_GAMEPAD_W0_IGNITION;
+        if (is_key_pressed(KEY_KPENTER, 0))     sb_data.dButtons[0] |= CXBX_SBC_GAMEPAD_W0_START;
+
+        if (is_key_pressed(KEY_DELETE, 0))         sb_data.dButtons[0] |= CXBX_SBC_GAMEPAD_W0_MULTIMONOPENCLOSE;
+        if (is_key_pressed(KEY_PAGEDOWN, 0))     sb_data.dButtons[0] |= CXBX_SBC_GAMEPAD_W0_MAINMONZOOMOUT;
+        if (is_key_pressed(KEY_PAGEUP, 0))    sb_data.dButtons[0] |= CXBX_SBC_GAMEPAD_W0_MAINMONZOOMIN;
+        if (is_key_pressed(KEY_HOME, 0))         sb_data.dButtons[0] |= CXBX_SBC_GAMEPAD_W0_MULTIMONMODESELECT;
+        if (is_key_pressed(KEY_END, 0))         sb_data.dButtons[0] |= CXBX_SBC_GAMEPAD_W0_MULTIMONSUBMONITOR;
+        if (is_key_pressed(KEY_INSERT, 0))         sb_data.dButtons[0] |= CXBX_SBC_GAMEPAD_W0_MULTIMONMAPZOOMINOUT;
+
+        //if (m->getWheel() > 0)                sb_data.dButtons[1] |= CXBX_SBC_GAMEPAD_W1_WEAPONCONMAIN;
+        //if (m->getWheel() < 0)                sb_data.dButtons[1] |= CXBX_SBC_GAMEPAD_W1_WEAPONCONSUB;
+
+
+        //if (is_key_pressed(KEY_X, 0))         sb_data.dButtons[0] |= CXBX_SBC_GAMEPAD_W0_RIGHTJOYLOCKON;
+
+
+     if (is_key_pressed(KEY_CAPS_LOCK, 0))
+        {
+           
+    
+    }
+        else{  
+      //Only runs this if mouse is available
+        if (m->available())
+        {
+            int32_t x, y;
+            x = sb_data.aimingX + m->getMouseX() * 100;
+            y = sb_data.aimingY + m->getMouseY() * 100;
+            if (x < 13000) x = 13000;
+            if (x > 51000) x = 51000;
+            if (y < 13000) y = 13000;
+            if (y > 51000) y = 51000;
+            sb_data.aimingX = x;
+            sb_data.aimingY = y;
+            //TU_LOG1("X: %i Y: %i, mX: %i, mY: %i\r\n", sb_data.aimingX, sb_data.aimingY, m->getMouseX(), m->getMouseY());
+            m->mouseDataClear();
+        }
+
+        }          
+        if (m->getButtons() & (1 << 0))       sb_data.dButtons[0] |= CXBX_SBC_GAMEPAD_W0_RIGHTJOYMAINWEAPON;
+        if (m->getButtons() & (1 << 1))       sb_data.dButtons[0] |= CXBX_SBC_GAMEPAD_W0_RIGHTJOYFIRE;
+        if (m->getButtons() & (1 << 2))       sb_data.dButtons[0] |= CXBX_SBC_GAMEPAD_W0_RIGHTJOYLOCKON;
+
+        }
+
+        if (!xid_send_report(index, &sb_data, sizeof(sb_data)))
+        {
+            TU_LOG1("[USBD] Error sending OUT report\r\n");
+        }
+    }
+
+    if (xid_get_report(index, &sb_feedback, sizeof(sb_feedback)))
+    {
+        static uint32_t print_timer = 0;
+
+        if (millis() - print_timer > 100)
+        {
+#if (0)
+        printf( "%c[2J", 27); //Clear terminal
+        printf( "%c[H", 27);  //Set cursor to home
+        printf("CockpitHatch:   %02x EmergeEject:   %02x\r\n", (sb_feedback.CockpitHatch_EmergencyEject >> 4) & 0x0F,
+                                                               (sb_feedback.CockpitHatch_EmergencyEject >> 0) & 0x0F);
+
+        printf("Start:          %02x Ignition:      %02x\r\n", (sb_feedback.Start_Ignition >> 4) & 0x0F,
+                                                               (sb_feedback.Start_Ignition >> 0) & 0x0F);
+
+        printf("MapZoomInOut:   %02x OpenClose:     %02x\r\n", (sb_feedback.MapZoomInOut_OpenClose >> 4) & 0x0F,
+                                                               (sb_feedback.MapZoomInOut_OpenClose >> 0) & 0x0F);
+
+        printf("SubMonModeSel:  %02x ModeSelect:    %02x\r\n", (sb_feedback.SubMonitorModeSelect_ModeSelect >> 4) & 0x0F,
+                                                               (sb_feedback.SubMonitorModeSelect_ModeSelect >> 0) & 0x0F);
+        
+        printf("MainMonZoomOut: %02x MainMonZoomIn: %02x\r\n", (sb_feedback.MainMonitorZoomOut_MainMonitorZoomIn >> 4) & 0x0F,
+                                                               (sb_feedback.MainMonitorZoomOut_MainMonitorZoomIn >> 0) & 0x0F);
+
+        printf("Manipulator:    %02x FSS:           %02x\r\n", (sb_feedback.Manipulator_ForecastShootingSystem >> 4) & 0x0F,
+                                                               (sb_feedback.Manipulator_ForecastShootingSystem >> 0) & 0x0F);
+
+        printf("Washing:        %02x LineColChange: %02x\r\n", (sb_feedback.Washing_LineColorChange >> 4) & 0x0F,
+                                                               (sb_feedback.Washing_LineColorChange >> 0) & 0x0F);
+
+        printf("Chaff:          %02x Extinguisher:  %02x\r\n", (sb_feedback.Chaff_Extinguisher >> 4) & 0x0F,
+                                                               (sb_feedback.Chaff_Extinguisher >> 0) & 0x0F);
+
+        printf("Override:       %02x TankDetach:    %02x\r\n", (sb_feedback.Override_TankDetach >> 4) & 0x0F,
+                                                               (sb_feedback.Override_TankDetach >> 0) & 0x0F);
+
+        printf("F1:             %02x NightScope:    %02x\r\n", (sb_feedback.F1_NightScope >> 4) & 0x0F,
+                                                               (sb_feedback.F1_NightScope >> 0) & 0x0F);
+
+        printf("F3:             %02x F2:            %02x\r\n", (sb_feedback.F3_F2 >> 4) & 0x0F,
+                                                               (sb_feedback.F3_F2 >> 0) & 0x0F);
+
+        printf("SubWeaponCtrl:  %02x MainWeapCtrl:  %02x\r\n", (sb_feedback.SubWeaponControl_MainWeaponControl >> 4) & 0x0F,
+                                                               (sb_feedback.SubWeaponControl_MainWeaponControl >> 0) & 0x0F);
+
+        printf("Comm1:          %02x MagChange:     %02x\r\n", (sb_feedback.Comm1_MagazineChange >> 4) & 0x0F,
+                                                               (sb_feedback.Comm1_MagazineChange >> 0) & 0x0F);
+
+        printf("Comm3:          %02x Comm2:         %02x\r\n", (sb_feedback.Comm3_Comm2 >> 4) & 0x0F,
+                                                               (sb_feedback.Comm3_Comm2 >> 0) & 0x0F);
+
+        printf("Comm5:          %02x Comm4:         %02x\r\n", (sb_feedback.Comm5_Comm4 >> 4) & 0x0F,
+                                                               (sb_feedback.Comm5_Comm4 >> 0) & 0x0F);
+
+        printf("GearR:          %02x Not used:      %02x\r\n", (sb_feedback.GearR_ >> 4) & 0x0F,
+                                                               (sb_feedback.GearR_ >> 0) & 0x0F);
+
+        printf("Gear1:          %02x GearN:         %02x\r\n", (sb_feedback.Gear1_GearN >> 4) & 0x0F,
+                                                               (sb_feedback.Gear1_GearN >> 0) & 0x0F);
+
+        printf("Gear3:          %02x Gear2:         %02x\r\n", (sb_feedback.Gear3_Gear2 >> 4) & 0x0F,
+                                                               (sb_feedback.Gear3_Gear2 >> 0) & 0x0F);
+
+        printf("Gear5:          %02x Gear4:         %02x\r\n", (sb_feedback.Gear5_Gear4 >> 4) & 0x0F,
+                                                               (sb_feedback.Gear5_Gear4 >> 0) & 0x0F);
+#endif
+        print_timer = millis();
+        }
+    }
+}
